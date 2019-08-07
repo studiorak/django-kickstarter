7eec4faa3889bc64235c65d566b7882456ae3f47
rakotovao@fr.jcdecaux.org <rakotovao@fr.jcdecaux.org>
2019-08-07 09:21:09 +0200

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/rakotovao/Documents/admintools/02-git_repo/kakebo/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
